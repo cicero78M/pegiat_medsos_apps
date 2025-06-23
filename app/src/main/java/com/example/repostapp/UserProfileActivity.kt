d9f05fa67f997be4d8225b0e5e098120dfb9d0de
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-23 10:49:22 +0700

    Add initial Android app skeleton

---
+++ app/src/main/java/com/example/repostapp/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.repostapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        // TODO: show user profile info
+    }
+}

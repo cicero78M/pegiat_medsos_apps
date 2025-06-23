5202ef6df512868a0c1d0b51afb8a2c4615d70b9
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-24 00:02:23 +0700

    Revamp profile layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,6 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 }

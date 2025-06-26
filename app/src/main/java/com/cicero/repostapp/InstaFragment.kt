ec3f16394bac58560a6870d423e3952568fbcb8a
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-26 15:21:29 +0700

    Revert "Add profile view on Instagram login"

--- app/src/main/java/com/cicero/repostapp/InstaFragment.kt
+++ app/src/main/java/com/cicero/repostapp/InstaFragment.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import android.widget.TextView
-import android.widget.ImageView
-import com.bumptech.glide.Glide
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlinx.coroutines.CoroutineScope
@@ -27,34 +24,6 @@ class InstaFragment : Fragment(R.layout.fragment_insta) {
                 out.writeObject(insta)
             }
         } catch (_: Exception) {
-        }
-    }
-
-    private fun showProfile(insta: Instagram4j, rootView: View) {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val result = insta.sendRequest(
-                    org.brunocvcunha.instagram4j.requests.InstagramSearchUsernameRequest(insta.username)
-                )
-                val user = result.user
-                withContext(Dispatchers.Main) {
-                    rootView.findViewById<TextView>(R.id.text_profile_username).text = "@" + user.username
-                    rootView.findViewById<TextView>(R.id.text_profile_name).text = user.full_name ?: ""
-                    rootView.findViewById<TextView>(R.id.stat_posts).text = user.media_count.toString()
-                    rootView.findViewById<TextView>(R.id.stat_followers).text = user.follower_count.toString()
-                    rootView.findViewById<TextView>(R.id.stat_following).text = user.following_count.toString()
-                    Glide.with(this@InstaFragment)
-                        .load(user.profile_pic_url)
-                        .placeholder(R.drawable.profile_avatar_placeholder)
-                        .circleCrop()
-                        .into(rootView.findViewById<ImageView>(R.id.image_avatar))
-                    rootView.findViewById<View>(R.id.profile_container).visibility = View.VISIBLE
-                }
-            } catch (_: Exception) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(requireContext(), "Gagal memuat profil", Toast.LENGTH_SHORT).show()
-                }
-            }
         }
     }
 
@@ -79,11 +48,10 @@ class InstaFragment : Fragment(R.layout.fragment_insta) {
 
                         val result = insta.login()
                         when {
-                            result.two_factor_info != null -> handleTwoFactor(insta, result, view)
+                            result.two_factor_info != null -> handleTwoFactor(insta, result)
                             result.challenge != null -> showCheckpoint(result.challenge.api_path)
                             else -> withContext(Dispatchers.Main) {
                                 saveSession(insta)
-                                showProfile(insta, view)
                                 Toast.makeText(requireContext(), "Login berhasil", Toast.LENGTH_SHORT).show()
                             }
                         }
@@ -101,7 +69,7 @@ class InstaFragment : Fragment(R.layout.fragment_insta) {
         }
     }
 
-    private suspend fun handleTwoFactor(insta: Instagram4j, result: org.brunocvcunha.instagram4j.requests.payload.InstagramLoginResult, rootView: View) {
+    private suspend fun handleTwoFactor(insta: Instagram4j, result: org.brunocvcunha.instagram4j.requests.payload.InstagramLoginResult) {
         withContext(Dispatchers.Main) {
             val input = EditText(requireContext())
             AlertDialog.Builder(requireContext())
@@ -114,7 +82,6 @@ class InstaFragment : Fragment(R.layout.fragment_insta) {
                             insta.finishTwoFactorLogin(input.text.toString(), result.two_factor_info.two_factor_identifier)
                             withContext(Dispatchers.Main) {
                                 saveSession(insta)
-                                showProfile(insta, rootView)
                                 Toast.makeText(requireContext(), "Login berhasil", Toast.LENGTH_SHORT).show()
                             }
                         } catch (_: Exception) {

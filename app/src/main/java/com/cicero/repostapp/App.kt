abeb67c7e15e78c41ca707c277f87cd0dba6ece7
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-02 15:12:44 +0700

    Initialize Facebook SDK on app startup

---
+++ app/src/main/java/com/cicero/repostapp/App.kt
@@ -0,0 +1,11 @@
+package com.cicero.repostapp
+
+import android.app.Application
+import com.facebook.FacebookSdk
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FacebookSdk.sdkInitialize(applicationContext)
+    }
+}

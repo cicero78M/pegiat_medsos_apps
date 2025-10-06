db5b7da9f052ec8bad35460bb11365da3799305f
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-10-06 19:58:00 +0700

    Ensure Gradle uses Java 17 toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,5 +14,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "CiceroReposter"
 include(":app")
